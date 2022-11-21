fc43f6fa8771175d5cd1dd67223a01d5e5294a2a
Taher Rampurawala <taherrampurawala5253@gmail.com>
2022-11-21 13:44:06 -0500

    matches functionality

--- main.py
+++ main.py
@@ -11,22 +11,42 @@ def utc_to_local(utc_dt):
 
 def load_data():
     world_cup_data = []
-
     file = open('worldcup_data.json')
     data = json.load(file)
+    return data
 
+def get_times(data):
+    time = list()
     for i in data:
         datetime_obj = datetime.strptime(i['DateUtc'], '%Y-%m-%d %H:%M:%S')
         local_time = utc_to_local(datetime_obj)
-        time = local_time.time()
-        # print(time)
+        # time[i] = local_time.time()
+        time.append(local_time)
+    return time
+
 
     # print(datetime.now().time())
+
+def find_matches(data, time):
+    current_date = datetime.now().date()
+    # current_date = current_time.date()
+
+    send_message = "Today's games are: \n"
+
+    for i in range(0,len(time)):
+        if time[i].date() == current_date:
+            send_message = send_message + data[i]['HomeTeam'] + " vs " + data[i]['AwayTeam'] + " " + time[i].strftime("%H:%M") + " EST" + "\n"
+    
+    return send_message
+
 
 def main():
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents = intents)
+
+    data = load_data()
+    time = get_times(data)
 
 
     @client.event
@@ -40,8 +60,9 @@ def main():
             return
 
         if message.content.startswith('!matches'):
-            time = datetime.now().time()
-            await message.channel.send(time)
+            # time = datetime.now().time()
+            send_message = find_matches(data,time)
+            await message.channel.send(send_message)
 
     client.run(get_token())
 
